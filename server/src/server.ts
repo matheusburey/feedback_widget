4ebde81d638064ec6cb7cc965501f0b8797a5f01
matheusburey <matheushburei.99@gmail.com>
2022-06-02 16:36:46 -0300

    feat: config to deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { router } from "./routes";
 
-const PORT = 3333
+const { PORT } = process.env;
 
 const app = express();
 
@@ -10,6 +10,6 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.listen(PORT || 3333, ()=>{
-	console.log(`runing PORT ${PORT}`)
+app.listen(PORT || 3333, () => {
+  console.log(`runing PORT ${PORT}`)
 })
